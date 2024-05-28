cbd617fcfd3a6b27bc4141ced62fc1e12ccf5401
saurabh prajapati <saurabh20prajapati@gmail.com>
2024-05-28 22:13:47 +0530

    Add project files.

---
+++ Blogging.Web/Models/Domain/BlogPost.cs
@@ -0,0 +1,18 @@
+﻿namespace Blogging.Web.Models.Domain
+{
+    public class BlogPost
+    {
+        public Guid Id { get; set; }
+        public String Heading { get; set; }
+        public String PageTitle { get; set; }
+        public String Content { get; set; }
+        public String ShortDiscription { get; set; }
+        public String FeaturedImageUrl { get; set; }
+        public String UrlHandle { get; set; }
+        public DateTime PublishDate { get; set; }
+        public String Author { get; set; }
+        public bool IsVisible { get; set; }
+
+        public ICollection<Tag> Tags { get; set; }
+    }
+}
---
+++ Blogging.Web/Models/Domain/Tag.cs
@@ -0,0 +1,11 @@
+﻿namespace Blogging.Web.Models.Domain
+{
+    public class Tag
+    {
+        public Guid Id { get; set; }
+        public int Name { get; set; }
+        public int DisplayName { get; set; }
+
+        public ICollection<BlogPost> BlogPosts { get; set; }
+    }
+}
