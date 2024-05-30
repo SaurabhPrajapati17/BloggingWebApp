169ef98330275ba9925b4ab2882881d1f9f0a08c
saurabh prajapati <saurabh20prajapati@gmail.com>
2024-05-30 21:29:38 +0530

    First Commit

---
+++ Blogging.Web/DBContext/BlogDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Blogging.Web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blogging.Web.DBContext
+{
+    public class BlogDbContext: DbContext
+    {
+        public BlogDbContext(DbContextOptions options) : base(options) 
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
--- Blogging.Web/Program.cs
+++ Blogging.Web/Program.cs
@@ -1,7 +1,12 @@
+using Blogging.Web.DBContext;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<BlogDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("BlogDbConnectionString")));
 
 var app = builder.Build();
 
